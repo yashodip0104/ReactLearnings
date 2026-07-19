7c69aa4342a97b842de4f52e98ac19e2c8f524e2
Yashodip Rajput <yashodiprajput45@gmail.com>
2026-07-19 13:29:21 +0530

    get cart from the backend and displayed the quantity

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,6 +1,10 @@
 import './header.css'
 import { Link } from 'react-router'
-export function Header() {
+export function Header({ cart }) {
+    let totalQuantity = 0;
+    cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity
+    })
     return (
         <>
             <div className="header">
@@ -29,7 +33,7 @@ export function Header() {
 
                     <Link className="cart-link header-link" to="/checkout">
                         <img className="cart-icon" src="images/icons/cart-icon.png" />
-                        <div className="cart-quantity">3</div>
+                        <div className="cart-quantity">{totalQuantity}</div>
                         <div className="cart-text">Cart</div>
                     </Link>
                 </div>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,16 +5,21 @@ import { Header } from '../components/Header';
 
 export function HomePage() {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then((response) => {
                 setProducts(response.data)
             })
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                setCart(response.data)
+            })
     }, [])
     return (
         <>
             <title>HomePage</title>
-            <Header />
+            <Header cart={cart} />
             <div className="home-page">
                 <div className="products-grid">
                     {products.map((product) => {
