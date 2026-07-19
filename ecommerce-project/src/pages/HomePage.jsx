0b355f9584012239dc2db7cb430aa6e6fb75b3f7
Yashodip Rajput <yashodiprajput45@gmail.com>
2026-07-19 13:47:58 +0530

    added server cofig proxy for /api and /img

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -7,11 +7,11 @@ export function HomePage() {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
             .then((response) => {
                 setProducts(response.data)
             })
-        axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
             .then((response) => {
                 setCart(response.data)
             })
