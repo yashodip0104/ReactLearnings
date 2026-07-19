82dd2b25e400dcee2c1692d7c7d6749cc7c8ccbb
Yashodip Rajput <yashodiprajput45@gmail.com>
2026-07-19 13:02:13 +0530

    fetched data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,12 @@ import { Header } from '../components/Header';
 import { products } from '../../../starting-code/data/products'
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+        })
     return (
         <>
             <title>HomePage</title>
