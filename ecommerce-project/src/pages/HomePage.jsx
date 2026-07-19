836311055a48756b3645bacce9be150e114a6c01
Yashodip Rajput <yashodiprajput45@gmail.com>
2026-07-19 13:06:58 +0530

    used axios to fetch the data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,12 @@
+import axios from 'axios';
 import './HomePage.css';
 import { Header } from '../components/Header';
 import { products } from '../../../starting-code/data/products'
 
 export function HomePage() {
-    fetch('http://localhost:3000/api/products')
+    axios.get('http://localhost:3000/api/products')
         .then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log(data);
+            response.data
         })
     return (
         <>
